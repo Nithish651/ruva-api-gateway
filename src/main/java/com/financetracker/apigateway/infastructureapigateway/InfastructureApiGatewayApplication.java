56ab7330da343716501f1e0a7ae3d5e8040662a2
Nithish651 <nithish651@gmail.com>
2023-12-19 20:46:14 +0530

    Initial commit

---
+++ src/main/java/com/financetracker/apigateway/infastructureapigateway/InfastructureApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.financetracker.apigateway.infastructureapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfastructureApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfastructureApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financetracker/apigateway/infastructureapigateway/InfastructureApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.apigateway.infastructureapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfastructureApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
